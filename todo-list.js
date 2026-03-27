dbae363369374add6d51be75f1dd9307242a9a8c
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-03-27 19:48:33 +0100

    add delete btn to each list for i can remove it

--- todo-list.js
+++ todo-list.js
@@ -4,7 +4,16 @@ const itemList = document.getElementById("item-list");
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
   const li = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "delete-btn text-white bg-red-500 px-2 rounded  ";
+  li.className = "flex justify-between";
+  deleteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    li.remove();
+  });
   li.textContent = itemInput.value;
   itemList.appendChild(li);
+  li.appendChild(deleteBtn);
   itemInput.value = "";
 });
